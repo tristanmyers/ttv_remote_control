275cb308aab6a0e7b2699e6aa0c3e921444906c2
tristanmyers <tristanmyers.developer@gmail.com>
2021-05-31 15:00:06 -0400

    grabbing needed elements from twitch channel page

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,20 +1,68 @@
-// background scipt will execute content.js as needed
-
-// i dont know if i will be using playwritght.
-// const { chromium } = require('playwright');
-
-// (async () => {
-//     const browser = await chromium.launch({ headless: false });
-//     const page = await browser.newPage();
-//     await page.goto('https://www.twitch.tv/strawberryhacker');
-//     await page.click('button.ixBesj');
-//     await browser.close();
-// })();
-
-// play/pause button class name = ixBesj
-
-// send get request for button action\
-// const response = fetch('http://localhost:8000/send_action');
-// console.log(response); 
-
 console.log("content script here, yo");
+
+// these elements children have unreliable classnames so select their children via indexes
+let playerControlsLeft = document.querySelectorAll('.player-controls__left-control-group');
+let playerControlsRight = document.querySelectorAll('.player-controls__right-control-group');
+
+// elements we will need for controlling the player 
+let channelPlayer = {
+	player: document.querySelectorAll('.player-controls'),
+	controls: {
+		container: document.querySelectorAll('.player-controls'),
+		leftGroup: {
+			button: playerControlsLeft[0].children[0].firstChild,
+			volume: 'coming soon'
+		},
+		rightGroup: {
+			theaterMode: 'coming soon',
+			fullscreen: 'coming soon'
+		}
+	}
+};
+
+// create button to test controlling the player with third party functions
+// REMOVE AFTER TESTING EXTENSION
+let testControl = document.createElement('button');
+testControl.setAttribute('type', 'button');
+testControl.setAttribute('name', 'textControlPlay');
+testControl.setAttribute('value', 'textControlPlay');
+testControl.style.width = '100px';
+testControl.style.height = '60px';
+testControl.style.backgroundColor = 'white';
+testControl.style.color = 'black';
+
+document.querySelector('.channel-root__info').appendChild(testControl);
+
+// get state of the play/pause button and change it
+let currentPlayerState = channelPlayer.controls.leftGroup.button.attributes['data-a-player-state'].value;
+
+function changePlayerState () {
+	try {
+
+		if (currentPlayerState == 'playing') {
+			currentPlayerState = 'paused';
+			console.log(`changed player state`);
+
+		} else {
+			currentPlayerState = 'playing';
+			console.log(`changed player state`);
+		}
+
+	} catch (err) {
+		console.error(`Error changing player state: ${err}`);
+	}
+};
+
+
+
+// content script only runs once so it's not updating to change state of the player
+testControl.addEventListener('click', () => {
+
+	if (currentPlayerState == 'playing') {
+		channelPlayer.controls.leftGroup.button.setAttribute('data-a-player-state', 'paused');
+		console.log(`if changed player state`);
+	} else {
+		channelPlayer.controls.leftGroup.button.setAttribute('data-a-player-state', 'playing');
+		console.log(`else changed player state`);
+	}	
+});
