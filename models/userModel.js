37acb1f7fea627e1ef8e140018375164bd39839e
tristanmyers <tristanmyers.developer@gmail.com>
2021-06-12 17:13:54 -0400

    add db stuff to model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,9 @@
-//functionality & logics related to the Database like
-// insert, fetch, update, delete queries. 
+//functionality & logics related to the Database like insert, fetch, update, delete queries. 
+const { MongoClient } = require('mongodb'); 
+
+// accessing mongodb
+const url = `mongodb+srv://admin:${process.env.MONGODB_PASS}@ttvrccluster.qpvms.mongodb.net/recentActions?retryWrites=true&w=majority`;
+const client = new MongoClient(url, { useUnifiedTopology: true });
 
 module.exports = {
     createUser: function() {
